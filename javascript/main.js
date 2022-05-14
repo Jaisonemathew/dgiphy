25e5b039df17a4864321eb523c4d3fba11e67557
Jaisonemathew <90517885+Jaisonemathew@users.noreply.github.com>
2022-05-14 15:36:37 +0530

    added files

---
+++ javascript/main.js
@@ -0,0 +1,70 @@
+
+// 1. grab the input value
+
+document.querySelector(".js-go").addEventListener('click', function(){
+
+	var input = document.querySelector("input").value;
+		pushToAPI(input);
+
+
+
+});
+
+document.querySelector(".js-userinput").addEventListener('keyup', function(e){
+
+	var input = document.querySelector("input").value;
+
+	if(e.which === 13) {
+		pushToAPI(input);
+	}
+
+});
+
+
+
+// 2. do the data stuff with the API
+
+function pushToAPI(input) {
+
+	var url = "http://api.giphy.com/v1/gifs/search?q="+ input +"&api_key=JoIWpzae1OdAPwa8qLuiIiWcwT1WSyAR"
+
+	console.log(url);
+
+	//AJAX Request
+	var GiphyAJAXCall = new XMLHttpRequest();
+	GiphyAJAXCall.open('Get', url);
+	GiphyAJAXCall.send();
+
+	GiphyAJAXCall.addEventListener('load', function (e) {
+			
+		var data = e.target.response;
+		pushToDOM(data);
+
+	});
+
+}
+
+
+
+//3. return and show the GIFs
+
+function pushToDOM(data) {
+
+	var container = document.querySelector(".js-container");
+
+	var response = JSON.parse(data);
+
+	var imageUrls = response.data;
+
+	container.innerHTML = "";
+
+	imageUrls.forEach(function(image){
+
+	var gifUrl = image.images.fixed_height.url;
+
+	container.innerHTML += "<img src=\"" + gifUrl + "\" class=\"container-image\">";	
+
+	});
+}
+
+
