eba140952c70a1a64941db101f6c3d3334f6e05e
Jaisonemathew <90517885+Jaisonemathew@users.noreply.github.com>
2022-05-14 15:43:07 +0530

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,21 @@
-
 // 1. grab the input value
 
-document.querySelector(".js-go").addEventListener('click', function(){
+document.querySelector(".js-go").addEventListener('click', function () {
 
-	var input = document.querySelector("input").value;
-		pushToAPI(input);
+    var input = document.querySelector("input").value;
+    pushToAPI(input);
 
 
 
 });
 
-document.querySelector(".js-userinput").addEventListener('keyup', function(e){
+document.querySelector(".js-userinput").addEventListener('keyup', function (e) {
 
-	var input = document.querySelector("input").value;
+    var input = document.querySelector("input").value;
 
-	if(e.which === 13) {
-		pushToAPI(input);
-	}
+    if (e.which === 13) {
+        pushToAPI(input);
+    }
 
 });
 
@@ -26,21 +25,21 @@ document.querySelector(".js-userinput").addEventListener('keyup', function(e){
 
 function pushToAPI(input) {
 
-	var url = "http://api.giphy.com/v1/gifs/search?q="+ input +"&api_key=JoIWpzae1OdAPwa8qLuiIiWcwT1WSyAR"
+    var url = "http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=JoIWpzae1OdAPwa8qLuiIiWcwT1WSyAR"
 
-	console.log(url);
+    console.log(url);
 
-	//AJAX Request
-	var GiphyAJAXCall = new XMLHttpRequest();
-	GiphyAJAXCall.open('Get', url);
-	GiphyAJAXCall.send();
+    //AJAX Request
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open('Get', url);
+    GiphyAJAXCall.send();
 
-	GiphyAJAXCall.addEventListener('load', function (e) {
-			
-		var data = e.target.response;
-		pushToDOM(data);
+    GiphyAJAXCall.addEventListener('load', function (e) {
 
-	});
+        var data = e.target.response;
+        pushToDOM(data);
+
+    });
 
 }
 
@@ -50,21 +49,19 @@ function pushToAPI(input) {
 
 function pushToDOM(data) {
 
-	var container = document.querySelector(".js-container");
+    var container = document.querySelector(".js-container");
 
-	var response = JSON.parse(data);
+    var response = JSON.parse(data);
 
-	var imageUrls = response.data;
+    var imageUrls = response.data;
 
-	container.innerHTML = "";
+    container.innerHTML = "";
 
-	imageUrls.forEach(function(image){
+    imageUrls.forEach(function (image) {
 
-	var gifUrl = image.images.fixed_height.url;
+        var gifUrl = image.images.fixed_height.url;
 
-	container.innerHTML += "<img src=\"" + gifUrl + "\" class=\"container-image\">";	
+        container.innerHTML += "<img src=\"" + gifUrl + "\" class=\"container-image\">";
 
-	});
+    });
 }
-
-
